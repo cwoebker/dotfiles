0dea25db11f0a3b116e0e423a33bfe1c0113ed65
cwoebker <cwoebker@gmail.com>
2015-03-09 10:51:08 -0300

    Added some helpful aliases

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -1,1 +1,4 @@
 alias reload!='. ~/.zshrc'
+alias pystall='python setup.py install'
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'";
+alias vup="vagrant up && vagrant provision && vagrant ssh"
