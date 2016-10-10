cd3d50cf56acf01bb76342cc614da9a077d6371d
Cecil Woebker <cwoebker@gmail.com>
2016-10-10 17:04:37 +0200

    Remove bootable creation script

--- osx/bootable.sh
+++
@@ -1,1 +0,0 @@
-sudo /Applications/Install\ OS\ X\ Yosemite.app/Contents/Resources/createinstallmedia --volume /Volumes/NATHANS --applicationpath /Applications/Install\ OS\ X\ Yosemite.app --nointeraction
