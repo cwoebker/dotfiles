42e488a4a17d6e5db95d32777024d6bbda833698
Cecil Woebker <cwoebker@gmail.com>
2016-10-10 17:02:22 +0200

    Merge OS X Aliases in main file

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias pystall='python setup.py install'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'";
 alias vup="vagrant up && vagrant provision && vagrant ssh"
+alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
