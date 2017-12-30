5bbb601b6e89735edd4918d1c87ec8cc34531bb8
Cecil Woebker <cwoebker@gmail.com>
2017-12-30 18:54:54 +0100

    Update dotfiles for 2018

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cwoebker's dotfiles
 
 These are my dotfiles feel free to use.
+
 ---
 
 ## Installation
@@ -14,21 +15,21 @@ Installing my dotfiles is simple:
 
 ...
 
-## Contribute
-
-Fork and contribute!
-
 ## TODO
 
 - Plugin section, in this readme
 
----
+## Contribute
 
-Question's and suggestions, feel free to shoot me an email <me@cwoebker.com>
-
-Follow [@cwoebker](http://twitter.com/cwoebker)
+[Fork and contribute!](https://github.com/cwoebker/dotfiles)
 
 ---
 
-Copyright (c) 2012, Cecil Woebker.
+For questions and suggestions, feel free to shoot me an email at <me@cwoebker.com>.
+
+Also, follow or tweet [@cwoebker](https://twitter.com/cwoebker).
+
+---
+
+Copyright (c) 2012-2018, Cecil Woebker.
 License: BSD (see LICENSE for details)
--- shell/functions.zsh
+++ shell/functions.zsh
@@ -34,3 +34,242 @@ function extract() {
 		echo "'$1' is not a valid file"
 	fi
 }
+
+# ==================================================================
+# = Functions =
+# ==================================================================
+# Show man page in Preview.app.
+# $ manp cd
+function manp {
+  local page
+  if (( $# > 0 )); then
+    for page in "$@"; do
+      man -t "$page" | open -f -a Preview
+    done
+  else
+    print 'What manual page do you want?' >&2
+  fi
+}
+
+# Show current Finder directory.
+function finder {
+  osascript 2>/dev/null <<EOF
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell
+EOF
+}
+
+# Gets password from OS X Keychain.
+# $ get-pass github
+function get-pass() {
+  keychain="$HOME/Library/Keychains/login.keychain"
+  security -q find-generic-password -g -l $@ $keychain 2>&1 |\
+    awk -F\" '/password:/ {print $2}';
+}
+
+# Opens file in EDITOR.
+function edit() {
+  local dir=$1
+  [[ -z "$dir" ]] && dir='.'
+  $EDITOR $dir
+}
+alias e=edit
+
+# Execute commands for each file in current directory.
+function each() {
+  for dir in *; do
+    # echo "${dir}:"
+    cd $dir
+    $@
+    cd ..
+  done
+}
+
+# Find files and exec commands at them.
+# $ find-exec .coffee cat | wc -l
+# # => 9762
+function find-exec() {
+  find . -type f -iname "*${1:-}*" -exec "${2:-file}" '{}' \;
+}
+
+# Better find(1)
+function ff() {
+  find . -iname "*${1:-}*"
+}
+
+# Count code lines in some directory.
+# $ loc py js css
+# # => Lines of code for .py: 3781
+# # => Lines of code for .js: 3354
+# # => Lines of code for .css: 2970
+# # => Total lines of code: 10105
+function loc() {
+  local total
+  local firstletter
+  local ext
+  local lines
+  total=0
+  for ext in $@; do
+    firstletter=$(echo $ext | cut -c1-1)
+    if [[ firstletter != "." ]]; then
+      ext=".$ext"
+    fi
+    lines=`find-exec "*$ext" cat | wc -l`
+    lines=${lines// /}
+    total=$(($total + $lines))
+    echo "Lines of code for ${fg[blue]}$ext${reset_color}: ${fg[green]}$lines${reset_color}"
+  done
+  echo "${fg[blue]}Total${reset_color} lines of code: ${fg[green]}$total${reset_color}"
+}
+
+# Show how much RAM application uses.
+# $ ram safari
+# # => safari uses 154.69 MBs of RAM.
+function ram() {
+  local sum
+  local items
+  local app="$1"
+  if [ -z "$app" ]; then
+    echo "First argument - pattern to grep from processes"
+  else
+    sum=0
+    for i in `ps aux | grep -i "$app" | grep -v "grep" | awk '{print $6}'`; do
+      sum=$(($i + $sum))
+    done
+    sum=$(echo "scale=2; $sum / 1024.0" | bc)
+    if [[ $sum != "0" ]]; then
+      echo "${fg[blue]}${app}${reset_color} uses ${fg[green]}${sum}${reset_color} MBs of RAM."
+    else
+      echo "There are no processes with pattern '${fg[blue]}${app}${reset_color}' are running."
+    fi
+  fi
+}
+
+# $ size dir1 file2.js
+function size() {
+  # du -sh "$@" 2>&1 | grep -v '^du:' | sort -nr
+  du -shck "$@" | sort -rn | awk '
+      function human(x) {
+          s="kMGTEPYZ";
+          while (x>=1000 && length(s)>1)
+              {x/=1024; s=substr(s,2)}
+          return int(x+0.5) substr(s,1,1)
+      }
+      {gsub(/^[0-9]+/, human($1)); print}'
+}
+
+# $ git log --no-merges --pretty=format:"%ae" | stats
+# # => 514 a@example.com
+# # => 200 b@example.com
+function stats() {
+  sort | uniq -c | sort -r
+}
+
+# Shortcut for searching commands history.
+# hist git
+function hist() {
+  history 0 | grep $@
+}
+
+# $ aes-enc file.zip
+function aes-enc() {
+  openssl enc -aes-256-cbc -e -in $1 -out "$1.aes"
+}
+
+# $ aes-dec file.zip.aes
+function aes-dec() {
+  openssl enc -aes-256-cbc -d -in $1 -out "${1%.*}"
+}
+
+# Converts a.mkv to a.m4v.
+function mkv2mp4() {
+  for file in "$@"; do
+    ffmpeg -i $file -map 0 -c copy "${file%.*}.m4v"
+  done
+}
+
+function mkv2mp4_1() {
+  for file in "$@"; do
+    ffmpeg -i $file -map 0:0 -map 0:1 -c copy -c:s mov_text "${file%.*}.m4v"
+  done
+}
+
+function mkv2mp4_2() {
+  for file in "$@"; do
+    ffmpeg -i $file -map 0:0 -map 0:2 -c copy -c:s mov_text "${file%.*}.m4v"
+  done
+}
+
+function mkv2mp4_3() {
+  for file in "$@"; do
+    ffmpeg -i $file -map 0:0 -map 0:3 -c copy -c:s mov_text "${file%.*}.m4v"
+  done
+}
+
+function jpgs2mp4() {
+  ffmpeg -pattern_type glob -i "*.JPG" -c:v libx264 -pix_fmt yuv420p output.mp4
+}
+
+function slowdown_mp4() { # 1/40 right now
+  for file in "$@"; do
+    ffmpeg -i $file -filter:v "setpts=0.025*PTS" -an "slowdown_${file}"
+  done
+}
+
+# Adds subs from a.srt to a.m4v.
+function addsubs() {
+  for file in "$@"; do
+    local raw="${file%.*}"
+    local old="$raw.m4v"
+    local new="$raw-sub.m4v"
+    ffmpeg -i $old -i $file -map 0 -map 1 -c copy -c:s mov_text $new
+    mv $new $old
+    rm $file
+  done
+}
+
+
+# Shortens GitHub URLs. By Sorin Ionescu <sorin.ionescu@gmail.com>
+function gitio() {
+  local url="$1"
+  local code="$2"
+
+  [[ -z "$url" ]] && print "usage: $0 url code" >&2 && exit
+  [[ -z "$code" ]] && print "usage: $0 url code" >&2 && exit
+
+  curl -s -i 'http://git.io' -F "url=$url" -F "code=$code"
+}
+
+# Monitor IO in real-time (open files etc).
+function openfiles() {
+  sudo dtrace -n 'syscall::open*:entry { printf("%s %s",execname,copyinstr(arg0)); }'
+}
+
+# 4 lulz.
+function compute() {
+  while true; do head -n 100 /dev/urandom; sleep 0.1; done \
+    | hexdump -C | grep "ca fe"
+}
+
+# Load 8 cores at once.
+function maxcpu() {
+  dn=/dev/null
+  yes > $dn & yes > $dn & yes > $dn & yes > $dn &
+  yes > $dn & yes > $dn & yes > $dn & yes > $dn &
+}
+
+# $ retry ping google.com
+function retry() {
+  echo Retrying "$@"
+  $@
+  sleep 1
+  retry $@
+}
+
+# Open curr dir in preview.app.
+function preview() {
+  local item=$1
+  [[ -z "$item" ]] && item='.'
+  open $1 -a 'Preview'
+}
