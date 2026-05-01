6ed7d66a9d95b35ddebc2c503c6c2b7b89114261
Cecil Wöbker <me@cwoebker.com>
2026-05-01 23:03:02 +0200

    Modernize tmux setup with TPM and session persistence
    - shell/tmux/tmux.conf: replace deprecated *-fg/bg/attr options with
      unified *-style (post-2.9 syntax); switch default-terminal to
      tmux-256color with RGB overrides for true color; wire vim-style
      copy-mode-vi bindings (v/V/y) with pbcopy integration
    - shell/tmux/theme.sh, theme_symbols.sh: refresh powerline glyphs
    - TPM as a submodule at tpm/, symlinked into ~/.tmux/plugins via
      install.conf.yaml so a fresh `./install` lands tmux-sensible,
      tmux-resurrect, and tmux-continuum without any manual prefix+I.

--- shell/tmux/theme.sh
+++ shell/tmux/theme.sh
@@ -1,67 +1,45 @@
 #### COLOUR
 
-# tm_prefix="☕ "
-# tm_prefix="♨︎ "
-tm_prefix="❏"
 tm_color_active=colour39
 tm_color_inactive=colour24
-tm_color_feature=colour24
-tm_color_music=colour24
-tm_active_border_color=colour39
 
 color_status_bg=colour234
 fmt_status_transparent="#[fg=$color_status_bg]"
 fmt_status_plain="#[fg=colour245]"
 fmt_status_bar_white="$fmt_status_transparent#[bg=colour254,bold]"
 
-# separators
-tm_separator_left_bold="⮂"
-tm_separator_left_thin="⮃"
-tm_separator_right_bold="⮀"
-tm_separator_right_thin="⮁"
+# Powerline separators (require Powerline-patched font in iTerm2)
 
-# default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg $color_status_bg
-set-option -g status-attr default
+# Status bar
+set -g status-style "fg=$tm_color_active,bg=$color_status_bg"
 
-# pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
+# Pane borders
+set -g pane-border-style "fg=$tm_color_inactive"
+set -g pane-active-border-style "fg=$tm_color_active"
 
-# message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+# Message text (overridden here to match theme)
+set -g message-style "fg=$tm_color_active,bg=default"
 
-# pane number display
-set-option -g display-panes-active-colour $tm_color_active
-set-option -g display-panes-colour $tm_color_inactive
+# Pane number display
+set -g display-panes-active-colour $tm_color_active
+set -g display-panes-colour $tm_color_inactive
 
-# clock
-set-window-option -g clock-mode-colour $tm_color_active
+# Clock
+setw -g clock-mode-colour $tm_color_active
 
-tm_date="$fmt_status_plain⮃ %R ⮃ %d %b" #"#[fg=$tm_color_inactive] %R %d %b"
-tm_host="$fmt_status_bar_white #h" #"#[fg=$tm_color_feature,bold]#h"
-tm_session_name="$fmt_status_bar_white #S" #" #[fg=$tm_color_feature,bold]$tm_prefix #S"
+tm_date="$fmt_status_plain %R  %d %b"
+tm_host="$fmt_status_bar_white #h"
+tm_session_name="$fmt_status_bar_white #S"
 
 tm_color_status_white="#[fg=colour254,bg=colour234,nobold]"
 
-# Custom status bar
-# Powerline symbols: ⮂ ⮃ ⮀ ⮁ ⭤
 set -g status-left-length 32
 set -g status-right-length 150
 set -g status-interval 5
 
-set -g status-left $tm_session_name' '$tm_color_status_white'⮀'
+# Status-left ends with arrow transitioning white→blue (active window color)
+set -g status-left "$tm_session_name#[fg=colour254,bg=colour39,nobold]"
+set -g status-right "$tm_date $tm_color_status_white$tm_host "
 
-set -g status-right $tm_date' '$tm_color_status_white'⮂'$tm_host' '
-
-# default window title colors
-#set -g window-status-format "$fmt_status_transparent#[bg=colour238]⮀#[fg=colour15,bg=colour238,bold] #I $fmt_status_transparent⮁#[fg=colour15,bg=colour238,bold] #W #[fg=colour238,bg=colour234,nobold]⮀"
-set -g window-status-format "$fmt_status_plain #I ⮁ #W"
-
-# active window title colors
-set -g window-status-current-format "$fmt_status_transparent#[bg=colour39]⮀#[noreverse,bold] #I ⮁ #W #[fg=colour39,bg=colour234,nobold]⮀"
-
-
+set -g window-status-format "$fmt_status_plain #I  #W"
+set -g window-status-current-format "#{?#{==:#{window_index},0},#[fg=colour16],$fmt_status_transparent#[bg=colour39]#[fg=colour16]}#[bg=colour39,bold] #I  #W #[fg=colour39,bg=colour234,nobold]"
---
+++ shell/tmux/theme_symbols.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Powerline symbols as variables
+export LEFT_BOLD=""
+export LEFT_THIN=""
+export RIGHT_BOLD=""
+export RIGHT_THIN=""
