9dd990828dc29a6b6763e607b39394bab612426a
Cecil Wöbker <me@cwoebker.com>
2026-05-25 14:27:13 +0200

    Update XDG config for zsh and npm

--- shell/config.zsh
+++ shell/config.zsh
@@ -4,6 +4,9 @@ export HISTFILE=$XDG_STATE_HOME/zsh/history
 export HISTSIZE=32768
 export SAVEHIST=$HISTSIZE
 
+# Redirect macOS Apple-zsh per-session history out of $HOME.
+export SHELL_SESSIONS_DIR=$XDG_STATE_HOME/zsh/sessions
+
 unsetopt correct_all
 setopt RM_STAR_WAIT
 setopt EXTENDED_HISTORY  # add timestamps to history
