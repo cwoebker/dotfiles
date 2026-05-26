a9c90ab090a19465a4f6d731d1e67ed5a6afb1f7
Cecil Wöbker <me@cwoebker.com>
2026-05-26 15:12:01 +0200

    Move xdg data to spec default location

--- shell/path.zsh
+++ shell/path.zsh
@@ -3,7 +3,7 @@
 
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.data
+export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
 
 export CUSTOM_APP_DIR=$HOME/.app
